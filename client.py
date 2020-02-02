d02293f35b3a10f8e7bcbd0a1986920cf6923f63
KaryM <35468047+KaryM711@users.noreply.github.com>
2020-02-02 21:54:24 +0100

    Update client.py

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 from random import*
 from threading import Thread
 import pickle
+import pygame_gui
 
 #COLORS:
 BLACK = (0,0,0)
@@ -14,9 +15,9 @@ BLUE = (10,10,150)
 #Game settings
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-FPS = 30
+FPS = 60
 #
-name = "user" + str(randint(0, 10000) + randint(0, 100))
+name = "player" + str(randint(0, 10000) + randint(0, 100))
 X, Y = 0.0, 0.0
 Health = 100.0
 
@@ -32,8 +33,16 @@ print("Connected to the server.")
 s.send(name.encode('UTF-8'))
 
 pygame.init()
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 8)
+
 surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("M711Platform")
+
+manager = pygame_gui.UIManager((800, 600))
+
+'''hello_button = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((350, 275), (100, 50)),
+                                             manager=manager)'''
 
 def gameLoop():
     while True:
@@ -53,8 +62,17 @@ def gameLoop():
                     s.send('D'.encode('UTF-8'))
                 elif(event.key == pygame.K_DOWN):
                     s.send('C'.encode('UTF-8'))
+            manager.process_events(event)
+        manager.update(pygame.time.Clock().tick(FPS))
+    
         for player in players:
-            pygame.draw.rect(surface, WHITE, (player['X'], player['Y'], 50,50))
+            pygame.draw.rect(surface, BLUE, (player['X'], player['Y'], 50,50))
+            pygame.draw.rect(surface, BLACK, (player['X'], player['Y']-10, 50,9))
+            pygame.draw.rect(surface, RED, (player['X'], player['Y']-10, int(player['H']/2),9))
+            textsurface = myfont.render(player['name'], False, (0, 0, 0))
+            surface.blit(textsurface,(player['X'], player['Y']))
+        manager.draw_ui(surface)
+
         pygame.display.update()
         pygame.time.Clock().tick(FPS)
 def recvMessages():
