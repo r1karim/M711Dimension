1c87ae2d8d9e933ca0f2cf776d7f2789e45095d1
KaryM <35468047+KaryM711@users.noreply.github.com>
2020-02-03 22:43:06 +0100

    Add files via upload

--- client.py
+++ client.py
@@ -3,7 +3,42 @@ import socket
 from random import*
 from threading import Thread
 import pickle
-import pygame_gui
+from Libraries import GUI_TextEntry as Ti
+from Libraries import ptext
+import time
+
+class spritesheet:
+    def __init__(self, filename, cols, rows):
+        self.sheet = pygame.image.load(filename).convert_alpha()
+        
+        self.cols = cols
+        self.rows = rows
+        self.totalCellCount = cols * rows
+        
+        self.rect = self.sheet.get_rect()
+        w = self.cellWidth = int(self.rect.width / cols)
+        h = self.cellHeight = int(self.rect.height / rows)
+        hw, hh = self.cellCenter = (int(w / 2), int(h / 2))
+        
+        self.cells = list([(index % cols * w, int(index / cols) * h, w, h) for index in range(self.totalCellCount)])
+        self.handle = list([
+            (0, 0), (-hw, 0), (-w, 0),
+            (0, -hh), (-hw, -hh), (-w, -hh),
+            (0, -h), (-hw, -h), (-w, -h),])
+        
+    def draw(self, surface, cellIndex, x, y, handle = 0):
+        surface.blit(self.sheet, (x + self.handle[handle][0], y + self.handle[handle][1]), self.cells[cellIndex])
+
+#
+SOUTH = 0
+WEST = 1
+NORTH = 2
+EAST = 3
+
+NORMAL = 0
+TEXTCHAT = 1
+PLAYERSLIST = 2
+gameState = NORMAL
 
 #COLORS:
 BLACK = (0,0,0)
@@ -11,7 +46,7 @@ WHITE = (255,255,255)
 GREEN = (10,150,10)
 RED = (150,10,10)
 BLUE = (10,10,150)
-
+TEST = (0,0,0,150)
 #Game settings
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -22,6 +57,9 @@ X, Y = 0.0, 0.0
 Health = 100.0
 
 players = []
+
+chatLog = "Welcome to M711 platform!"
+
 
 #Connection to the server.
 IP = '127.0.0.1'
@@ -35,26 +73,27 @@ s.send(name.encode('UTF-8'))
 pygame.init()
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 8)
-
+textChatFont = pygame.font.SysFont('Arial',15)
 surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("M711Platform")
 
-manager = pygame_gui.UIManager((800, 600))
+playerSheet = spritesheet("george.png", 4,4)
 
-'''hello_button = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((350, 275), (100, 50)),
-                                             manager=manager)'''
+
+
+textinput = Ti.TextInput("", "none",20,True,WHITE,WHITE)
 
 def gameLoop():
+    global gameState
     while True:
         surface.fill(GREEN)
-
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if(event.type == pygame.QUIT):
                 pygame.quit()
                 sys.exit()
             elif(event.type == pygame.KEYDOWN):
                 if(event.key == pygame.K_RIGHT):
-                    #print("meow")
                     s.send('A'.encode('UTF-8'))
                 elif(event.key == pygame.K_LEFT):
                     s.send('B'.encode('UTF-8'))
@@ -62,20 +101,59 @@ def gameLoop():
                     s.send('D'.encode('UTF-8'))
                 elif(event.key == pygame.K_DOWN):
                     s.send('C'.encode('UTF-8'))
-            manager.process_events(event)
-        manager.update(pygame.time.Clock().tick(FPS))
-    
+                elif(event.key == pygame.K_t):
+                    if(gameState == NORMAL):
+                        gameState = TEXTCHAT
+                        time.sleep(0.3)
+                        textinput.input_string  = ""
+
+                elif(event.key==pygame.K_TAB):
+                    if(gameState == NORMAL or gameState == TEXTCHAT):
+                        gameState = PLAYERSLIST
+                    else:
+                        gameState=NORMAL
+                elif(event.key == pygame.K_RETURN):
+                    if(gameState == TEXTCHAT):
+                        gameState = NORMAL
+                        msg = textinput.get_text()
+                        print(msg)
+                        msg = 'E'+msg
+                        msg = msg.encode('UTF-8')
+                        textinput.input_string  = ''
+                        s.send(msg)
+
         for player in players:
-            pygame.draw.rect(surface, BLUE, (player['X'], player['Y'], 50,50))
+            #pygame.draw.rect(surface, BLUE, (player['X'], player['Y'], 50,50))
+            if(player['D'] == SOUTH):
+                playerSheet.draw(surface,0,player['X'], player['Y'])
+            elif(player['D'] == WEST):
+                playerSheet.draw(surface,1,player['X'], player['Y'])
+            elif(player['D'] == EAST):
+                playerSheet.draw(surface,3,player['X'], player['Y'])
+            elif(player['D'] == NORTH):
+                playerSheet.draw(surface,2,player['X'], player['Y'])
+
             pygame.draw.rect(surface, BLACK, (player['X'], player['Y']-10, 50,9))
             pygame.draw.rect(surface, RED, (player['X'], player['Y']-10, int(player['H']/2),9))
-            textsurface = myfont.render(player['name'], False, (0, 0, 0))
-            surface.blit(textsurface,(player['X'], player['Y']))
-        manager.draw_ui(surface)
-
+            textsurface = myfont.render(player['name'], False, WHITE)
+            surface.blit(textsurface,(player['X'], player['Y']-10))
+        if(gameState == TEXTCHAT):
+            pygame.draw.rect(surface, BLACK, (10,10, SCREEN_WIDTH-20, 25))
+            textinput.update(events)
+            surface.blit(textinput.get_surface(), (10, 10))
+        elif(gameState == PLAYERSLIST):
+            BOX_WIDTH = 200
+            BOX_HEIGHT = 300
+            pygame.draw.rect(surface, TEST,((SCREEN_WIDTH/2)-(BOX_WIDTH/2), (SCREEN_HEIGHT/2)-(BOX_HEIGHT/2), BOX_WIDTH,BOX_HEIGHT))
+            playerlisttext = "name\t\tScore\n"
+            for player in players:
+                playerlisttext+=player['name']+'\t\t'+str(player['S'])+'\n'
+            ptext.draw(playerlisttext,((SCREEN_WIDTH/2)-(BOX_WIDTH/2), (SCREEN_HEIGHT/2)-(BOX_HEIGHT/2)), color=WHITE)
+        ptext.draw(chatLog,(10,SCREEN_HEIGHT-150), color=BLUE)
         pygame.display.update()
         pygame.time.Clock().tick(FPS)
 def recvMessages():
+    global chatLog
     global players
     while True:
         try:
@@ -89,8 +167,10 @@ def recvMessages():
             if(temp[0] == 'F'):
                 temp = message.decode('UTF-8')
                 print(temp[1:])
+                chatLog+=temp[1:]
         except:
             pass
-ServerCommunicator = Thread(target=recvMessages)
-ServerCommunicator.start()
-gameLoop()
+if(__name__=='__main__'): 
+    ServerCommunicator = Thread(target=recvMessages)
+    ServerCommunicator.start()
+    gameLoop()
--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import time
 
 print("Launching the server...")
 
+#
+SOUTH = 0
+WEST = 1
+NORTH = 2
+EAST = 3
+
 class Player:
     def __init__(self, ID,NAME, IP, HEALTH, X,Y):
         self.id = ID
@@ -13,6 +19,8 @@ class Player:
         self.Health = HEALTH
         self.x = X
         self.y = Y
+        self.score = 0
+        self.direction = SOUTH
     def GetPlayerPosition(self):
         return self.x,self.y
     def GetPlayerName(self):
@@ -21,6 +29,8 @@ class Player:
         return self.Health
     def GetPlayerIp(self):
         return self.ip
+    def GetPlayerScore(self):
+        return self.score
     def printInformation(self):
         print("Name: " + self.name + " IP: " + str(self.ip) + " Health: " + str(self.Health) + " X: " +str(self.x) +" Y: "+str(self.y))
 clients = []
@@ -31,39 +41,60 @@ s = socket.socket()
 s.bind((IP,PORT))
 s.listen(50)
 s.settimeout(0.0001)
+
+def SendAllPlayersMessage(message):
+    for client in clients:
+        client.id.send(message.encode('UTF-8'))
+
 def CommunicateWithPlayer(__client__):
     while True:
         try:
-            m = __client__.id.recv(1)
+            m = __client__.id.recv(1024)
             m = m.decode("UTF-8")
             if(m[0] == 'A'):
                 __client__.x+=5 #right
+                __client__.direction = EAST
                 UpdatePlayers()
+
             elif(m[0] == 'B'):
                 __client__.x-=5 #left
+                __client__.direction = WEST
                 UpdatePlayers()
+
             elif(m[0] == 'C'):
                 __client__.y+=5 #down
+                __client__.direction = SOUTH
                 UpdatePlayers()
+
             elif(m[0] == 'D'):
                 __client__.y-=5 #up
+                __client__.direction = NORTH
                 UpdatePlayers()
+
+            elif(m[0] == 'E'):
+                if('/damage' in m[1:8]):
+                    __client__.Health-=25
+                    UpdatePlayers()
+
+                    pMessage = "F\n"+__client__.name+" has been hurt."
+                    time.sleep(0.1)
+                    SendAllPlayersMessage(pMessage)
+                    print('meeeeeeow')
+                else:
+                    pMessage = "F\n"+__client__.name+": "+m[1:]
+                    SendAllPlayersMessage(pMessage)
+                    print(pMessage)
+
         except:
             pass
 def UpdatePlayers():
     players = []
     for client in clients:
-        playerDict = {'name': client.name, 'X': client.x, 'Y':client.y, 'H':client.Health}
+        playerDict = {'name': client.name, 'X': client.x, 'Y':client.y, 'H':client.Health, 'S':client.score, 'D': client.direction}
         players.append(playerDict)
     for e in clients:
         obj = pickle.dumps(players)
-        print('pickled')
-        #message = 'E'.encode('UTF-8')
-        #message = message + obj
         e.id.send(obj)
-        print('sent.')
-        e.printInformation()
-
 def HandleConnections():
     while True:
         try:
@@ -75,17 +106,15 @@ def HandleConnections():
             clients.append(player)
             thread = Thread(target=CommunicateWithPlayer, args=[player])
             thread.start()
-            message = 'F'+player.name + " has joined the game."
+            message = 'F\n'+player.name + " has joined the game."
             print(player.name + " has connected. IP: " + str(player.ip))
-            for c in clients:
-                c.id.send(message.encode('UTF-8'))
+            SendAllPlayersMessage(message)
             time.sleep(2)
             UpdatePlayers()
         except:
             pass
 
-ConnectionHandler = Thread(target=HandleConnections)
-#PlayerMovementHandler = Thread(target=CommunicateWithPlayers)
-ConnectionHandler.start()
-#PlayerMovementHandler.start()
-print("Server has started.")
+if(__name__=='__main__'):
+    ConnectionHandler = Thread(target=HandleConnections)
+    ConnectionHandler.start()
+    print("Server has started.")
