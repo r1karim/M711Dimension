6bd2369510d0ef8b6fd759fcd5e31b4dabc42383
KaryM <35468047+KaryM711@users.noreply.github.com>
2020-02-26 22:02:48 +0100

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -12,20 +12,26 @@ class widget:
 		self.y = y
 		self.width = width
 		self.height = height
+		self.type = ''
+		self.isSelected = False
 		widget.widgets.append(self)
 
 class Button(widget):
-	def __init__(self, text, x,y,width,height, color):
+	def __init__(self,ID,x,y,width, height, text,color):
+		super().__init__(ID,x,y,width,height)
 		self.text = text
 		self.color = color
+		self.type = 'button'
 
 class Textarea(widget):
-	def __init__(self,x,y,width,height,maxlen,txtcolor, bgcolor, pholder):
+	def __init__(self,ID,x,y,width, height, pholder,maxlen,txtcolor, bgcolor):
+		super().__init__(ID,x,y,width,height)
+		self.placeholder = pholder
 		self.maxlength = maxlen
 		self.textcolor = txtcolor
 		self.color = bgcolor
 		self.text = ''
-		self.placeholder = pholder
+		self.type = 'textarea'
 		
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -51,7 +57,7 @@ s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 s.bind(('127.0.0.1', 60310))
 bytes_ = '$__J711__$_4EVA'.encode('UTF-8')
 servers = []
-txt = Textarea(100,10,100,25,24,BLACK, BLUE, 'Username')
+txt = Textarea('username_input',100,10,100,25,'username',24,BLACK, BLUE)
 
 def RefreshList():
 	global servers
@@ -63,41 +69,48 @@ def RefreshList():
 		print('ERROR: Could not contact the master server.')
 
 def gameloop():
-	playButton = Button('Play', 30 + SCREEN_WIDTH-200,10, 100,25, GREEN)
-	refreshButton = Button('Refresh',30 + SCREEN_WIDTH-200,40, 100,25, GREEN)
+	playButton = Button('play_button', 30 + SCREEN_WIDTH-200,10, 100,25, 'Play',GREEN)
+	refreshButton = Button('refresh_button', 30 + SCREEN_WIDTH-200,40, 100,25, 'Refresh',GREEN)
 
 	while True:
 		surface.fill(GREY)
 		events = pygame.event.get()
 		for event in events:
-			if(event.type == pygame.QUIT):
+			if event.type == pygame.QUIT:
 				pygame.quit()
-			elif(event.type == pygame.MOUSEBUTTONUP):
+			elif event.type == pygame.MOUSEBUTTONUP:
 				x,y = pygame.mouse.get_pos()
-				for btn in Button.widgets:
-					if x >= btn.x and x <= btn.x + btn.width and y >= btn.y and y <= btn.y+btn.height:
-						if btn.text == 'Play':
-							pass
-						elif btn.text == 'Refresh':
-							time.sleep(1)
-							RefreshList()
-				for txtar in Textarea.widgets:
-					if x >= txtar.x and x <= txtar.x+txtar.width and y >= txtar.y and y <= txtar.y + txtar.height:
-						txtar.isSelected = True
+
+				for wdgt in widget.widgets:
+					if wdgt.type == 'button':
+						if x >= wdgt.x and x <= wdgt.x + wdgt.width and y >= wdgt.y and y <= wdgt.y+wdgt.height:
+							if wdgt.id == 'play_button':
+								pass
+							elif wdgt.text == 'refresh_button':
+								time.sleep(1)
+								RefreshList()
+					elif wdgt.type == 'textarea':
+						if x >= wdgt.x and x <= wdgt.x+wdgt.width and y >= wdgt.y and y <= wdgt.y + wdgt.height:
+							wdgt.isSelected = True
+
 		pygame.draw.rect(surface, LIGHTBLUE, (10,40, SCREEN_WIDTH-200, 25))
 		ptext.draw('hostname\tplayers', (12,42), fontsize=25)
-		for btn in Button.widgets:
-			pygame.draw.rect(surface, btn.color, (btn.x,btn.y, btn.width, btn.height))
-			ptext.draw(btn.text, (btn.x+int(btn.width/4), btn.y+ int(btn.height / 4)))
-		for txtar in Textarea.widgets:
-			pygame.draw.rect(surface, txtar.color, (txtar.x, txtar.y, txtar.width, txtar.height))
-			ptext.draw(txtar.text, (txtar.x + int(txtar.width/4), txtar.y + int(txtar.height/4)))
-			if(txtar.isSelected == False):
-				ptext.draw(txtar.placeholder, (txtar.x + int(txtar.width/5), txtar.y + int(txtar.height/4)), color=GREEN, fontsize=20)
+		for wdgt in widget.widgets:
+			if(wdgt.type == 'button'):
+				pygame.draw.rect(surface, wdgt.color, (wdgt.x,wdgt.y, wdgt.width, wdgt.height))
+				ptext.draw(wdgt.text, (wdgt.x+int(wdgt.width/4), wdgt.y+ int(wdgt.height / 4)))
+			elif(wdgt.type == 'textarea'):
+				pygame.draw.rect(surface, wdgt.color, (wdgt.x, wdgt.y, wdgt.width, wdgt.height))
+				ptext.draw(wdgt.text, (wdgt.x + int(wdgt.width/4), wdgt.y + int(wdgt.height/4)))
+				if wdgt.isSelected == False:
+					ptext.draw(wdgt.placeholder, (wdgt.x + int(wdgt.width/5), wdgt.y + int(wdgt.height/4)), color=GREEN, fontsize=20)
+				else:
+					pass
 		for srv in servers:
 			serverid = servers.index(srv) + 1
 			pygame.draw.rect(surface, LIGHTBLUE, (10,40 + (serverid*30),SCREEN_WIDTH-200,25))
 			ptext.draw(srv['hostname']+'\t0/'+str(srv['maxplayers']), (14, 44 + (serverid*30)), fontsize=20)
+
 		pygame.display.update()
 		pygame.time.Clock().tick(FPS)
 if __name__ == '__main__':
